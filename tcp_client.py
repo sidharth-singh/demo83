5ff6af482c26dd3ca3c580c6af157b49b9e05002
sidharth-singh <sidharthsingh96@live.com>
2017-02-16 16:02:21 +0530

    new update

--- tcp_client.py
+++ tcp_client.py
@@ -8,4 +8,6 @@ conn=socket.socket()
 socket.send(str.encode("GET / HTTP/1.1\rl\nHost: google.co.in\r\n\r\n"))
 
 response= socket.recv(2048)
+print("Response recieved from: ")
 print(str(response, "utf-8"))
+
