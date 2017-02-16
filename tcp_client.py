c6ffa4b2ce334de53f4ca5f4dc760a2831e99faf
sidharth-singh <sidharthsingh96@live.com>
2017-02-16 15:58:32 +0530

    tcp_client

---
+++ tcp_client.py
@@ -0,0 +1,11 @@
+import socket
+
+host="www.google.co.in"
+port=80
+
+conn=socket.socket()
+
+socket.send(str.encode("GET / HTTP/1.1\rl\nHost: google.co.in\r\n\r\n"))
+
+response= socket.recv(2048)
+print(str(response, "utf-8"))
